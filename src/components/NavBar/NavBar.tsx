1c7f80178cb5586687e008f665c9b11bf5d12c08
para_slov <paraslovjm@gmail.com>
2021-03-17 21:25:15 +0300

    Lesson 20. NavLink was added to NavBar component. Also activeClassName was added to NavBar.module.css

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,22 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './NavBar.module.css'
 
 function NavBar() {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a href={"/profile"}>Profile</a>
+            <NavLink to={"/profile"} activeClassName={s.activeLink}>Profile</NavLink>
         </div>
         <div className={s.item}>
-            <a href={"/dialogs"}>Messages</a>
+            <NavLink to={"/dialogs"} activeClassName={s.activeLink}>Messages</NavLink>
         </div>
         <div className={s.item}>
-            <a href={"/news"}>News</a>
+            <NavLink to={"/news"} activeClassName={s.activeLink}>News</NavLink>
         </div>
         <div className={s.item}>
-            <a href={"/music"}>Music</a>
+            <NavLink to={"/music"} activeClassName={s.activeLink}>Music</NavLink>
         </div>
         <div className={s.item}>
-            <a href={"/settings"}>Settings</a>
+            <NavLink to={"/settings"} activeClassName={s.activeLink}>Settings</NavLink>
         </div>
     </nav>
 }
