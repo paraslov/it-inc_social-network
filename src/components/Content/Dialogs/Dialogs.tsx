19f8432a1b378613ca91f5bd5cd716f3ec46f5c8
paraslov <paraslovjm@gmail.com>
2021-04-01 08:30:05 +0300

    Lesson 21. Dialogs.tsx component layout.

--- src/components/Content/Dialogs/Dialogs.tsx
+++ src/components/Content/Dialogs/Dialogs.tsx
@@ -1,9 +1,31 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import s from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs Page
+        <div className={s.contentWrapper}>
+            <div className={s.userDialogs}>
+                <NavLink to={'/dialogs/1'} className={s.dialogItem}
+                         activeClassName={s.active}>Kitomo Natsuro</NavLink>
+                <NavLink to={'/dialogs/2'} className={s.dialogItem}
+                         activeClassName={s.active}>Sikoko Segun</NavLink>
+                <NavLink to={'/dialogs/3'} className={s.dialogItem}
+                         activeClassName={s.active}> Kisyu Natsuro</NavLink>
+                <NavLink to={'/dialogs/4'} className={s.dialogItem}
+                         activeClassName={s.active}>Alex</NavLink>
+                <NavLink to={'/dialogs/5'} className={s.dialogItem}
+                         activeClassName={s.active}>Iniomo Ui</NavLink>
+                <NavLink to={'/dialogs/6'} className={s.dialogItem}
+                         activeClassName={s.active}>Hiroshi Kagawa</NavLink>
+                <NavLink to={'/dialogs/7'} className={s.dialogItem}
+                         activeClassName={s.active}>Xin Jao</NavLink>
+            </div>
+            <div className={s.userMessages}>
+                <div className={s.messageItem}>How's your samurai lessons?</div>
+                <div className={s.messageItem}>How are you, broh?</div>
+                <div className={s.messageItem}>Hi</div>
+            </div>
         </div>
     )
 }
